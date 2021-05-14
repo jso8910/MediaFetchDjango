9f7b67e59e83fc993bd82a9265d844b4a02f85cc
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-05-14 22:43:56 +0800

    Code quality bad, but home page of example works

--- exampleapp/urls.py
+++ exampleapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('add', views.add, name='add_category'),
+    path('delete', views.delete, name='delete_category'),
 ]
--- exampleapp/views.py
+++ exampleapp/views.py
@@ -1,15 +1,46 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from .models import Category
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'exampleapp/layout.html')
+@login_required
+def delete(request):
+    id = request.GET.get('id', None)
 
+    object = Category.objects.filter(id=id, user=User.objects.get(username=request.user.username))
+
+    if not object:
+        return JsonResponse({"error": "Id doesn't exist or doesn't belong to you"}, status=400)
+    
+    object.delete()
+
+    return JsonResponse({"message": "success"}, status=200)
+
+@login_required
+def add(request):
+    content = request.GET.get('category', None)
+
+    if not content:
+        return JsonResponse({"error": "category name missing"}, status=400)
+    
+    category = Category(category=content, user=User.objects.get(username=request.user.username))
+    category.save()
+    return JsonResponse({"message": "success", "id": category.id}, status=200)
+
+@login_required
+def index(request):
+    categories = Category.objects.filter(user=User.objects.get(username=request.user.username))
+    return render(request, 'exampleapp/index.html', {
+        'categories': categories,
+        'token': Token.objects.get(user=User.objects.get(username=request.user.username)).key,
+    })
+
+@login_required
 def search(request):
     if not request.GET.get('query', None):
         return HttpResponseRedirect(reverse('index'))
